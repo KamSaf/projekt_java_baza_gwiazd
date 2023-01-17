d460ba93c8955d9da13b8348ed76fafcc3d206df
bopaf <116653905+bopaf@users.noreply.github.com>
2023-01-17 17:26:55 +0100

    Gwiazdozbior class getters and setters

--- src/Gwiazdozbior.java
+++ src/Gwiazdozbior.java
@@ -1,4 +1,19 @@
+import java.util.ArrayList;
+
 public class Gwiazdozbior {
-    private String nazwa;
-    private String[] listaGwiazd;
+    private String nazwaGwiazdozbioru;
+    public String getNazwaGwiazdozbioru() {
+        return nazwaGwiazdozbioru;
+    }
+    public void setNazwaGwiazdozbioru(String nazwa) {
+        this.nazwaGwiazdozbioru = nazwa;
+    }
+
+    private ArrayList<String> listaGwiazd;
+    public ArrayList<String> getListaGwiazd() {
+        return listaGwiazd;
+    }
+    public void setListaGwiazd(ArrayList<String> lista) {
+        this.listaGwiazd = lista;
+    }
 }
